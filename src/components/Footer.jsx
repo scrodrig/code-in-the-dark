ce8cb829daf23cf92766e6324aaa699ad39c3671
Valentin Zapata <valentinsvt@hotmail.com>
2018-08-23 16:29:20 -0500

    add hit counter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,10 @@ class Footer extends React.Component {
             <a href="https://www.thoughtworks.com/es/" target="_blank" rel="noopener noreferrer">
                 <img className="small-logo" src="content/tw-logo-white.png" alt="Thoughtworks" />
             </a>
+            <br/>
+            <a href="http://www.webfreecounter.com/" target="_blank" style={{ visibility: "hidden"}}>
+                <img src="http://www.cutercounter.com/hit.php?id=24551&nd=6&style=24" border="0" alt="visitor counter"/>
+            </a>
       </footer>
     );
   }
